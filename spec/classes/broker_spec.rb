23070c2f272be34494ff699feed0c542911054d6
Tim Meusel <tim@bastelfreak.de>
2020-12-21 17:25:12 +0100

    (#203) implement rspec-puppet-facts

---
+++ spec/classes/broker_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe 'choria' do
+
+  let :node do
+    'rspec.puppet.com'
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
+      context 'with all defaults' do
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
+  end
+end
