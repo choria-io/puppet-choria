e384f7731cca27236588be6612bf744991d4a00a
R.I.Pienaar <rip@devco.net>
2021-08-04 19:11:04 +0200

    (#260) rename jetstream adapter
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- types/adapters.pp
+++ types/adapters.pp
@@ -1,4 +1,4 @@
 type Choria::Adapters = Variant[
   Hash[String, Choria::Adapters::NatsStream, 0],
-  Hash[String, Choria::Adapters::JetStream, 0]
+  Hash[String, Choria::Adapters::ChoriaStreams, 0]
 ]
