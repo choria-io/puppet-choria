f9ce0903ecaab9d95c6f68b2c128521524767109
Yury Bushmelev <jay4mail@gmail.com>
2026-03-17 16:44:54 +0800

    (misc) add missing splay_start machine parameter

--- types/machinetype.pp
+++ types/machinetype.pp
@@ -2,6 +2,7 @@
 type Choria::MachineType = Struct[{
     "version"       => String,
     "initial_state" => String[1],
+    "splay_start"   => Optional[Integer[0]],
     "transitions"   => Array[Hash],
     "watchers"      => Array[Hash],
     "ensure"        => Optional[Enum["present", "absent"]],
